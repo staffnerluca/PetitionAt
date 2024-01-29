d8a7e2788e4498f4663e94d75d7ff6c9c28c93c9
Luca Staffner <staffnerluca@gmail.com>
2024-01-29 14:55:02 +0100

    implemented json sending json

--- Backend/main.go
+++ Backend/main.go
@@ -1,23 +1,43 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
 )
 
-type petition struct {
-	title        string
-	text         string
-	startingDate time.Time
-}
+func OnGetPetition(w http.ResponseWriter, r *http.Request) {
+	pet := Petition{
+		Title:        "Title",
+		Text:         "my Text",
+		StartingDate: time.Now(),
+		PolScore:     2,
+		Attorney:     "Gustav",
+		WebLink:      "myLink",
+	}
 
-func OnIndex(w http.ResponseWriter, r *http.Request) {
+	jData, err := json.Marshal(pet)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
+	w.Header().Set("Content-Type", "application/json")
+
+	_, err = w.Write(jData)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
 
 func main() {
 	fmt.Println("Server starting")
 
-	http.HandleFunc("/", OnIndex)
+	http.HandleFunc("/GetPetition", OnGetPetition)
+	err := http.ListenAndServe(":8081", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
--- Backend/pettition.go
+++ Backend/pettition.go
@@ -4,11 +4,11 @@ import (
 	"time"
 )
 
-type petition struct {
-	title        string
-	text         string
-	startingDate time.Time
-	polScore     int
-	attorney     string
-	webLink      string
+type Petition struct {
+	Title        string    `json:"title"`
+	Text         string    `json:"text"`
+	StartingDate time.Time `json:"startingDate"`
+	PolScore     int       `json:"polScore"`
+	Attorney     string    `json:"attorney"`
+	WebLink      string    `json:"webLink"`
 }
